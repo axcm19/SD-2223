83c5ca0f08e3f108c9796a81523a267e5a9452d5
ZeNoFaria <jdlf2309@gmail.com>
2022-12-14 16:05:31 +0000

    alteracao de lista para hashmap

--- src/Mapa.java
+++ src/Mapa.java
@@ -5,6 +5,8 @@ O mapa tem uma lista de posições que são compostas por um par(x, y) onde x, y
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -19,6 +21,20 @@ public class Mapa {
         public int coord_x;
         public int coord_y;
 
+        public  Posicao(){
+            this.coord_x = 0;
+            this.coord_y = 0;
+        }
+
+        public Posicao(int x,int y){
+            this.coord_x = x;
+            this.coord_y = y;
+        }
+
+        public Posicao(Posicao p){
+            this.coord_x = p.coord_x;
+            this.coord_y = p.coord_y;
+        }
         //faltam métodos
 
     }
@@ -27,8 +43,10 @@ public class Mapa {
 
     // tamanho do mapa = N*N
     private int N = 20;
-    private int numTroti = 36;
-    private List<Posicao> locais;
+    private int nsector = 16;
+    private int tamanhosector = 5;
+    private int sector = 0;
+    private HashMap<Integer, List<Posicao>> locais; //alterar o formato
     public ReentrantReadWriteLock lock_mapa = new ReentrantReadWriteLock();
 
     //inicia o mapa    nao sei se esta certo
@@ -36,16 +54,21 @@ public class Mapa {
         Posicao trotinete = new Posicao();
         int x = -1;
         int y = -1;
-        this.N = 20;
-        this.locais = new ArrayList<Posicao>();
-        while(x < N){
-            x = x + 3;
-            trotinete.coord_x = x;
-            while(y < N){
-                y = y + 3;
-                trotinete.coord_y = y;
-                locais.add(trotinete);
+        int i = 1;
+        List<Posicao> posicoes = new ArrayList<>();
+        this.locais = new HashMap<>();
+        while(i <= nsector){
+            while(x<tamanhosector){
+                x = x+2;
+                trotinete.coord_x = x;
+                while(y<tamanhosector){
+                    y = y+2;
+                    trotinete.coord_y = y;
+                    posicoes.add(trotinete);
+                }
             }
+            locais.put(i,posicoes);
+            i++;
         }
         Mapa mapa = new Mapa();
         mapa.locais = this.locais;
@@ -65,6 +88,7 @@ public class Mapa {
     }
 
     //pegar posiçao do user e tirar do mapa
+    //mudar para os hashmap
     public void reservaTrotinete(Posicao p,Mapa m){
         Posicao temp = new Posicao();
         temp.coord_x = 0;
@@ -78,15 +102,16 @@ public class Mapa {
     }
 
     //tira posicao inicial da lista de posiçoes do mapa e poe posiçao final na lista
-    public Mapa desloca(Posicao inicio,Posicao fim,Mapa m){
+    //mudar para os hashmap
+    public void desloca(Posicao inicio,Posicao fim,Mapa m){
         for(int i = 0;i<m.numTroti;i++){
             if(comparaPosicoes(inicio,m.locais.get(i))==true){
                 m.locais.remove(i);
             }
         }
         m.locais.add(fim);
-        //faltam coisas
-        return m;
+        int custo = calculaDistancia(inicio,fim);
+        //envia para o cliente o custo da viagem
     }
     //faltam métodos
 
